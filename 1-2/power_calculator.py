485e1c9b368919e4590d8f7662eb1ea53e4b18c5
peachily <peachily1108@gmail.com>
2025-07-18 16:17:25 +0900

    [1-2] 제곱계산기 완성

--- 1-2/power_calculator.py
+++ 1-2/power_calculator.py
@@ -1,10 +1,19 @@
-try:
-  number = float(input("제곱하고 싶은 숫자를 입력하세요: "))
-except ValueError:
-  print("Invalid number input")
+def main () :
+  try:
+    base = float(input("제곱하고 싶은 숫자를 입력하세요: "))
+  except ValueError:
+    print("Invalid number input")
 
-try:
+  try:
     exponent = int(input("제곱할 횟수를 정수로 입력하세요: "))
-except ValueError:
-   print("Invalid exponent input")
+  except ValueError:
+    print("Invalid exponent input")
 
+  result = 1
+  for _ in range(exponent):
+    result = result*base
+
+  print(result)
+
+if __name__ == "__main__":
+    main()
