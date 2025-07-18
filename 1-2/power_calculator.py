2bafcc051453a6cf6c248c86fea858423cc556c9
peachily <peachily1108@gmail.com>
2025-07-18 18:36:04 +0900

    [1-2] 음수 제곱 처리

--- 1-2/power_calculator.py
+++ 1-2/power_calculator.py
@@ -3,15 +3,21 @@ def main () :
     base = float(input("제곱하고 싶은 숫자를 입력하세요: "))
   except ValueError:
     print("Invalid number input")
+    return
 
   try:
     exponent = int(input("제곱할 횟수를 정수로 입력하세요: "))
   except ValueError:
     print("Invalid exponent input")
-
+    return
+  
   result = 1
-  for _ in range(exponent):
-    result = result*base
+  if exponent >= 0:
+    for _ in range(exponent):
+      result = result*base
+  else:
+    for _ in range(abs(exponent)):
+      result = result / base
 
   print(result)
 
